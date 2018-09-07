6ac45f9d24e71756ab3227736d49cef19306d162
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 20:41:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizbuzz(num)
-  if num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  elsif num % 15 == 0
-    return "FizzBuzz"
-  else
-    return "foo"
-  end
+
 end
